cad8561b67166670e8a1748a9db37b7fff85c490
Martin Vidner <mvidner@suse.cz>
2015-06-01 11:48:46 +0200

    Do not `include Yast` lest Term break Coveralls.
    class Yast::Term would clash with module Term from term-ansicolor.gem
    which is a dependency of coveralls.gem
    
    The error was:
    
    Formatter Coveralls::SimpleCov::Formatter failed with TypeError:
    Term is not a module
    (/var/lib/gems/2.1.0/gems/term-ansicolor-1.3.0/lib/term/ansicolor.rb:1:in `<top (required)>')

--- test/kdump_test.rb
+++ test/kdump_test.rb
@@ -2,40 +2,39 @@
 
 require_relative "./test_helper"
 
-include Yast
 Yast.import "Kdump"
 
-describe Kdump do
+describe Yast::Kdump do
   # allocated_memory is a string   in megabytes
   # total_memory     is an integer in megabytes
   describe "#ProposeAllocatedMemory" do
     context "when already proposed" do
       before(:each) do
-        Kdump.allocated_memory = "42"
+        Yast::Kdump.allocated_memory = "42"
       end
       it "proposes the current value" do
-        Kdump.ProposeAllocatedMemory
-        expect(Kdump.allocated_memory).to eq "42"
+        Yast::Kdump.ProposeAllocatedMemory
+        expect(Yast::Kdump.allocated_memory).to eq "42"
       end
     end
 
     context "when not yet proposed" do
       before(:each) do
-        Kdump.allocated_memory = "0"
+        Yast::Kdump.allocated_memory = "0"
       end
 
       context "when the proposal tool is not implemented yet" do
         before(:each) do
-          SCR.stub(:Execute).
-            with(path(".target.bash"), /^cp/).and_return(0)
-          SCR.stub(:Execute).
-            with(path(".target.bash_output"), /^kdumptool/).
+          Yast::SCR.stub(:Execute).
+            with(Yast::Path.new(".target.bash"), /^cp/).and_return(0)
+          Yast::SCR.stub(:Execute).
+            with(Yast::Path.new(".target.bash_output"), /^kdumptool/).
             and_return({"exit" => 1, "stdout" => "", "stderr" => "not there" })
         end
 
         it "proposes a positive integer" do
-          Kdump.ProposeAllocatedMemory
-          expect(Kdump.allocated_memory.to_i).to be > 0
+          Yast::Kdump.ProposeAllocatedMemory
+          expect(Yast::Kdump.allocated_memory.to_i).to be > 0
         end
       end
     end
