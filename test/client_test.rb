c4133ba03d655c4c0f263278f13f834075e2df9f
Gilson Souza <gsouzadossantos@suse.com>
2017-06-02 15:44:16 +0200

    Added tests to check memory allocation through commandline

---
+++ test/client_test.rb
@@ -0,0 +1,42 @@
+require_relative "./test_helper"
+require_relative "../src/clients/kdump"
+ 
+describe Yast::KdumpClient do
+ 
+  describe ".cmdKdumpStartup" do
+    let(:alloc_mem_high) {"100"}
+    let(:alloc_mem_low) {"50"}
+    
+    before(:each) do
+      subject.cmdKdumpStartup(options)
+    end
+   
+    context "When using alloc memory high and low" do
+      let(:options) {{"enable"=>"", "alloc_mem"=>"#{alloc_mem_low},#{alloc_mem_high}"}}
+    
+      it "sets the low and high values of  Kdump allocated_memory " do
+        expect(Yast::Kdump.allocated_memory).to eq({:low=>alloc_mem_low, 
+          :high=>alloc_mem_high})
+        expect(Yast::Kdump.add_crashkernel_param).to be true
+      end
+    end
+    
+    context "when using only alloc memory low" do
+      let(:options) {{"enable"=>"", "alloc_mem"=>"#{alloc_mem_low}"}}
+      
+      it "enables only the low value of Kdump allocated_memory " do
+        expect(Yast::Kdump.allocated_memory).to eq({:low=>alloc_mem_low, 
+          :high=>nil})
+        expect(Yast::Kdump.add_crashkernel_param).to be true
+      end
+    end
+    
+    context "When receive option disable" do
+      let(:options) {{"disable"=>""}}
+      
+      it "sets kdump to be disabled" do
+        expect(Yast::Kdump.add_crashkernel_param).to be false
+      end
+    end
+  end  
+end
