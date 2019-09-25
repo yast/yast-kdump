e8a2903ca5f6504ea038fe155478925a26237416
Josef Reidinger <jreidinger@suse.cz>
2019-09-25 12:37:24 +0200

    convert auto client from ycp client to object based client

---
+++ src/lib/kdump/clients/auto.rb
@@ -0,0 +1,74 @@
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+# Copyright (c) 2019 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# ------------------------------------------------------------------------------
+
+require "installation/auto_client"
+
+module Y2Kdump
+  module Clients
+    # Client to communicate with autoyast
+    class Auto < ::Installation::AutoClient
+      def initialize
+        textdomain "kdump"
+
+        Yast.import "Kdump"
+        Yast.import "Mode"
+        Yast.import "Progress"
+
+        Yast.include self, "kdump/wizards.rb" # needed for auto sequence
+      end
+
+      def import(profile)
+        Yast::Kdump.Import(profile)
+      end
+
+      def export
+        Yast::Kdump.Export
+      end
+
+      def summary
+        items = Yast::Kdump.Summary.map { |s| "<li>#{s}</li>" }
+        "<ul>#{items.join("\n")}</ul>"
+      end
+
+      def modified?
+        Yast::Kdump.GetModified
+      end
+
+      def modified
+        Yast::Kdump.SetModified
+      end
+
+      def reset
+        Yast::Kdump.Import({})
+      end
+
+      def change
+        KdumpAutoSequence()
+      end
+
+      def write
+        progress_orig = Yast::Progress.set(false)
+        Yast::Kdump.Write
+        Yast::Progress.set(progress_orig)
+      end
+
+      def read
+        progress_orig = Yast::Progress.set(false)
+        Yast::Kdump.Read
+        Yast::Progress.set(progress_orig)
+      end
+    end
+  end
+end
