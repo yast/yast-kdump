184b6c4edd08e5c945d2ebf4640e90b032d6bd10
David Díaz González <dgonzalez@suse.de>
2021-09-06 21:57:42 +0100

    Fix the kdump client unit test
    Using a dirty mocking for include/kdump/complex routines not relevant
    for tested methods.

--- test/client_test.rb
+++ test/client_test.rb
@@ -1,8 +1,27 @@
 require_relative "./test_helper"
-require_relative "../src/clients/kdump"
+require "kdump/clients/kdump"
+
 Yast.import "CommandLine"
+Yast.import "Kdump"
+
+# Let's "dirty-mock" Yast::KdumpComplexInclude to avoid not desired dialogs during test
+require_relative "../src/include/kdump/complex"
+module Yast::KdumpComplexInclude
+  def ReadDialog
+    true
+  end
+
+  def InstallPackages
+    true
+  end
+end
 
 describe Yast::KdumpClient do
+  subject { Yast::KdumpClient.new }
+
+  before do
+    subject.main
+  end
 
   describe ".cmdKdumpStartup" do
     let(:alloc_mem_high) { "100" }
@@ -12,40 +31,50 @@ describe Yast::KdumpClient do
       allow(Yast::Arch).to receive(:paravirtualized_xen_guest?).and_return(false)
     end
 
-    context "When using wrong inputs in alloc memory" do
-      let(:options) { { "enable" => "", "alloc_mem" => "#{alloc_mem_low}:#{alloc_mem_high}" } }
+    context "when receiving the 'enable' option" do
+      context "and using wrong inputs in alloc memory" do
+        let(:options) { { "enable" => "", "alloc_mem" => "#{alloc_mem_low}:#{alloc_mem_high}" } }
 
-      it "does not enable kdump and returns false" do
-        expect(subject.cmdKdumpStartup(options)).to be false
+        it "does not enable kdump and returns false" do
+          expect(subject.cmdKdumpStartup(options)).to be false
+        end
+      end
+
+      context "and using valid high and low inputs in alloc memory" do
+        let(:options) { { "enable" => "", "alloc_mem" => "#{alloc_mem_low},#{alloc_mem_high}" } }
+
+        it "sets the low and high values of Kdump allocated_memory " do
+          subject.cmdKdumpStartup(options)
+          expect(Yast::Kdump.allocated_memory).to eq(low: alloc_mem_low, high: alloc_mem_high)
+          expect(Yast::Kdump.add_crashkernel_param).to be true
+        end
+      end
+
+      context "and using a single input in alloc memory" do
+        let(:options) { { "enable" => "", "alloc_mem" => alloc_mem_low.to_s } }
+
+        it "enables only the low value of Kdump allocated_memory " do
+          subject.cmdKdumpStartup(options)
+          expect(Yast::Kdump.allocated_memory).to eq(low: alloc_mem_low, high: nil)
+          expect(Yast::Kdump.add_crashkernel_param).to be true
+        end
       end
     end
 
-    context "When using alloc memory high and low" do
-      let(:options) { { "enable" => "", "alloc_mem" => "#{alloc_mem_low},#{alloc_mem_high}" } }
-
-      it "sets the low and high values of  Kdump allocated_memory " do
-        subject.cmdKdumpStartup(options)
-        expect(Yast::Kdump.allocated_memory).to eq(low:  alloc_mem_low, high: alloc_mem_high)
-        expect(Yast::Kdump.add_crashkernel_param).to be true
-      end
-    end
-
-    context "when using only alloc memory low" do
-      let(:options) { { "enable" => "", "alloc_mem" => alloc_mem_low.to_s } }
-
-      it "enables only the low value of Kdump allocated_memory " do
-        subject.cmdKdumpStartup(options)
-        expect(Yast::Kdump.allocated_memory).to eq(low:  alloc_mem_low, high: nil)
-        expect(Yast::Kdump.add_crashkernel_param).to be true
-      end
-    end
-
-    context "When receives the 'disable' option" do
-      let(:options) { { "disable"=>"" } }
+    context "when receiving the 'disable' option" do
+      let(:options) { { "disable" => "", "alloc_mem" => alloc_mem_low.to_s } }
 
       it "sets kdump to be disabled" do
         subject.cmdKdumpStartup(options)
         expect(Yast::Kdump.add_crashkernel_param).to be false
+      end
+    end
+
+    context "when receiving neither, 'enable' nor 'disable' option" do
+      let(:options) { { "alloc_mem" => alloc_mem_low.to_s } }
+
+      it "returns false" do
+        expect(subject.cmdKdumpStartup(options)).to be false
       end
     end
   end
